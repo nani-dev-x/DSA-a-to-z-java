56ec689872f101da75cfc71d78f45d4999e1bf4b
p.yuvateja <yuvatejapeta560@gmail.com>
2026-08-05 22:14:18 +0530

    pattern 5

--- Patterns/pattern_5.java
+++ Patterns/pattern_5.java
@@ -4,7 +4,7 @@ class pattern_5
     {
          for(int i =0;i<n;i++)
         {
-            for(int j=0;j<n-i;j++)
+            for(int j=0;j<n-i+1;j++)
             {
                 System.out.print("*");
             }
