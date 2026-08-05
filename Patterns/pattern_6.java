287586ea1efa1d28a389c7a112635f4e3e991fbc
p.yuvateja <yuvatejapeta560@gmail.com>
2026-08-05 22:12:38 +0530

    pattern 6

---
+++ Patterns/pattern_6.java
@@ -0,0 +1,19 @@
+public class pattern_6 {
+    static void cases(int n)
+    {
+         for(int i =1;i<=n;i++)
+        {
+            for(int j=1;j<=n-i+1;j++)
+            {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        for(int i =5;i<=6;i++)
+        {
+            pattern_6.cases(i);
+        }
+    }
+}
