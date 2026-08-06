42e004bb05b5854aa187e1c00eb62e32fd4afc02
p.yuvateja <yuvatejapeta560@gmail.com>
2026-08-06 10:13:36 +0530

    pattern 8

---
+++ Patterns/pattern_8.java
@@ -0,0 +1,24 @@
+public class pattern_8 {
+    static void cases(int n)
+    {
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<i;j++)
+            {
+                System.out.print(" ");
+            }
+            for(int k=0;k<n*2-(2*i+1);k++)
+            {
+                System.out.print("*");
+            }
+             for(int l=0;l<i;l++)
+            {
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        pattern_8.cases(5);
+    }
+}
