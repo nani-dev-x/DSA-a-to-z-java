c920a98e807f0b93fc91a131da999baf55688674
p.yuvateja <yuvatejapeta560@gmail.com>
2026-08-06 09:44:21 +0530

    pattern 7

---
+++ Patterns/pattern_7.java
@@ -0,0 +1,25 @@
+public class pattern_7 {
+    static void cases(int n)
+    {
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n-(i+1);j++)
+            {
+                System.out.print(" ");
+            }
+           
+            for(int k=0;k<2*i+1;k++)
+            {
+                System.out.print("*");
+            }
+             for(int l=0;l<n-(i+1);l++)
+            {
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        pattern_7.cases(4);
+    }
+}
