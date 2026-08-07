a7afdc376357ed3de976c7cb6718a1d42e5bb3b1
p.yuvateja <yuvatejapeta560@gmail.com>
2026-08-07 22:04:16 +0530

    pattern 12

---
+++ Patterns/pattern_12.java
@@ -0,0 +1,21 @@
+public class pattern_12 {
+    public static void main(String[] args) {
+        int n =5;
+        for(int i=1;i<=n;i++)
+        {
+            for(int j =1;j<=i;j++)
+            {
+                System.out.print(j);
+            }
+            for(int k=0;k<2*n-i-i;k++)
+            {
+                System.out.print(" ");
+            }
+            for(int j =i;j>=1;j--)
+            {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+}
